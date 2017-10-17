642bc419a1c6d8c96cf67c997d9ef04fa1bdde49
Anas Laaroussi Alami <anaslaaroussialami@gmail.com>
2017-10-17 10:33:49 +0100

    (Fix)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProfComponent } from './prof/prof.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+
+
+
+const  appRoutes : Routes = [
+  {path:'professeur',
+  component: ProfComponent},
+  {path:'etudiant',
+  component: EtudiantComponent},
+  {path:'',
+  component:ConnexionComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfComponent,
+    EtudiantComponent,
+    ConnexionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
