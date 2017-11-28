af65ceee7a4dcea6e4eb3fc0413a0f97d4c9379d
Anas Laaroussi Alami <anaslaaroussialami@gmail.com>
2017-11-28 21:13:27 +0000

    Fix bugs

--- src/app/etudiant/etudiant.component.ts
+++ src/app/etudiant/etudiant.component.ts
@@ -115,22 +115,43 @@ getQuizs(id: number){
   if(this.dataSend !== undefined){
     this.data =  this.dataSend[id]
 
-    console.log(this.data[3])
+
      this.db.object('users/'+this.userUID+'/'+this.dateNow)
        .set( {
-              quiztitle : this.data[3]
+              quiztitle : this.dataSend[0][3]
+
        })
 
-
+console.log(this.dataSend)
 
   }
 }
 
 
   nextQuestion(idQuestion: number,idQuiz:number, reponse : any){
+// extraire la reponse du bouton radio already checked
+
+    let res  = '';
+    let tru = document.forms[1].true.checked
+    let fals = document.forms[1].false.checked
+    if(tru) {
+      res = 'true'
+    }
+
+    if(fals){
+      res = 'false'
+
+    }
+
+    // fin extraction => res est la reponse donne par le user dans check box
+
+
+
+
 
     let object = {}
-    object[idQuestion] = reponse
+
+    object[idQuestion] = res
 
     this.db.list('users/'+this.userUID+'/'+this.dateNow+"/responses")
    .push(object)
@@ -145,9 +166,9 @@ getQuizs(id: number){
     let reponses = this.dataSend[idQuiz][1];
 
     console.log('les reponses : ' + reponses)
-      this.response.push(reponse);
+      this.response.push(res);
 
-      if(reponse == reponses[idQuestion] ){ this.globalNote += 2
+      if(res == reponses[idQuestion] ){ this.globalNote += 2
         this.test = true;
 
 
@@ -159,7 +180,7 @@ getQuizs(id: number){
         this.test = false
       }
 
-    console.log(`Votre reponse : ${reponse}     !! La reponse correct ${reponses[idQuestion]}`)
+    console.log(`Votre reponse : ${res}     !! La reponse correct ${reponses[idQuestion]}`)
 
     this.idQuestion = idQuestion + 1;
 console.log(this.idQuestion)
@@ -188,15 +209,20 @@ console.log(this.quizByCategorie)
     let dataQuestions = [q.question1,q.question2,q.question3,q.question4,q.question5,q.question6,q.question7,q.question8,q.question9,q.question10]
     let dataAnswers = [q.answer1,q.answer2,q.answer3,q.answer4,q.answer5,q.answer6,q.answer7,q.answer8,q.answer9,q.answer10];
     let dataCategorie = q.categorie;
-    let dataTitle = q.title
+    let dataTitle = q.quizTitle
     this.dataSend.push([dataQuestions,dataAnswers,dataCategorie,dataTitle])
 
     this.compteur.push(i);
 
   }
-
+console.log(this.dataSend)
 
 }})}
+
+
+//sass
+
+
 
 }
 
